d25e6624807d6e42912745c3ca60d0d2e65dfb3a
Bavo Van Achte <bvn@melexis.com>
2017-06-09 09:59:53 +0200

    Fixed regex to also accept warnings without a valid line number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import basename, dirname, join, splitext
 from setuptools import find_packages, setup
 
 PROJECT_URL = 'https://github.com/melexis/warnings-plugin'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 
 def read(*names, **kwargs):
