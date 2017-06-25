f5817ec9f553dfc9cd88d79436f97b0a906ffb69
Crt Mori <cmo@melexis.com>
2017-06-25 20:51:15 +0200

    Setting and checking limits within class
    Added some unit tests

--- src/mlx/warnings.py
+++ src/mlx/warnings.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import sys
+import errno
 
 DOXYGEN_WARNING_REGEX = r"(?:(?:((?:[/.]|[A-Za-z]:).+?):(-?\d+):\s*([Ww]arning|[Ee]rror)|<.+>:-?\d+(?::\s*([Ww]arning|[Ee]rror))?): (.+(?:\n(?!\s*(?:[Nn]otice|[Ww]arning|[Ee]rror): )[^/<\n][^:\n][^/\n].+)*)|\s*([Nn]otice|[Ww]arning|[Ee]rror): (.+))$"
 doxy_pattern = re.compile(DOXYGEN_WARNING_REGEX)
@@ -14,7 +15,7 @@ junit_pattern = re.compile(JUNIT_WARNING_REGEX)
 
 class WarningsPlugin:
 
-    def __init__(self, sphinx, doxygen, junit):
+    def __init__(self, sphinx = False, doxygen = False, junit = False):
         # type: (boolean, boolean, boolean) -> None
         '''
         Function for initializing the parsers
@@ -44,17 +45,30 @@ class WarningsPlugin:
         for checker in self.checkerList:
             checker.check(line)
 
-    def limit_maximum(self, maximum):
-        if self.warn_min > maximum:
+    def set_maximum(self, maximum):
+        if self.warn_min == 0:
+            self.warn_max = maximum
+            return 0
+        elif self.warn_min > maximum:
             print("Invalid argument: mininum limit ({min}) is higher than maximum limit ({max}). Cannot enter {value}". format(min=self.warn_min, max=self.warn_max, value=maximum))
+            return errno.EINVAL
         else:
             self.warn_max = maximum
+            return 0
 
-    def limit_minimum(self, minimum):
+    def get_maximum(self):
+        return self.warn_max
+
+    def set_minimum(self, minimum):
         if minimum > self.warn_max:
             print("Invalid argument: mininum limit ({min}) is higher than maximum limit ({max}). Cannot enter {value}". format(min=self.warn_min, max=self.warn_max, value=minimum))
+            return errno.EINVAL
         else:
             self.warn_min = minimum
+            return 0
+
+    def get_minimum(self):
+        return self.warn_min
 
     def return_count(self):
         self.count = 0
@@ -64,11 +78,11 @@ class WarningsPlugin:
         return self.count
 
     def return_check_limits(self):
-        if self.warn_count > self.warn_max:
-            print("Number of warnings ({count}) is higher than the maximum limit ({max}). Returning error code 1.".format(count=self.warn_count, max=self.warn_max))
+        if self.count > self.warn_max:
+            print("Number of warnings ({count}) is higher than the maximum limit ({max}). Returning error code 1.".format(count=self.count, max=self.warn_max))
             return 1
-        elif self.warn_count < self.warn_min:
-            print("Number of warnings ({count}) is lower than the minimum limit ({min}). Returning error code 1.".format(count=self.warn_count, min=self.warn_min))
+        elif self.count < self.warn_min:
+            print("Number of warnings ({count}) is lower than the minimum limit ({min}). Returning error code 1.".format(count=self.count, min=self.warn_min))
             return 1
         else:
             print("Number of warnings ({count}) is between limits {min} and {max}. Well done.".format(count=self.count, min=self.warn_min, max=self.warn_max))
---
+++ tests/test_limits.py
@@ -0,0 +1,71 @@
+import errno
+from unittest import TestCase
+
+from mlx.warnings import WarningsPlugin
+
+
+class TestLimits(TestCase):
+
+    def test_set_maximum(self):
+        warnings = WarningsPlugin(False, True, False)
+        for x in range(0,10):
+            self.assertEqual(warnings.set_maximum(x), 0)
+            self.assertEqual(warnings.get_maximum(), x)
+
+    def test_set_minimum(self):
+        warnings = WarningsPlugin(False, True, False)
+        # Setting minimum is tricky - we need to max out maximum
+        self.assertEqual(warnings.set_maximum(11), 0)
+        for x in range(0, 10):
+            self.assertEqual(warnings.set_minimum(x), 0)
+            self.assertEqual(warnings.get_minimum(), x)
+
+    def test_set_minimum_fail(self):
+        warnings = WarningsPlugin(False, True, False)
+        self.assertEqual(warnings.set_maximum(5), 0)
+        for x in range(1, 5):
+            self.assertEqual(warnings.set_minimum(x), 0)
+            self.assertEqual(warnings.get_minimum(), x)
+
+        for x in range(6, 10):
+            self.assertEqual(warnings.set_minimum(x), errno.EINVAL)
+
+    def test_return_values_maximum_decrease(self):
+        warnings = WarningsPlugin(False, True, False)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 1)
+        warnings.set_maximum(1)
+        self.assertEqual(warnings.return_check_limits(), 0)
+        warnings.set_maximum(0)
+        self.assertEqual(warnings.return_check_limits(), 1)
+
+    def test_return_values_maximum_increase(self):
+        warnings = WarningsPlugin(False, True, False)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 1)
+        warnings.check('testfile.c:12: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 2)
+        warnings.set_maximum(1)
+        self.assertEqual(warnings.return_check_limits(), 1)
+        warnings.set_maximum(2)
+        self.assertEqual(warnings.return_check_limits(), 0)
+
+    def test_return_values_minimum_increase(self):
+        warnings = WarningsPlugin(False, True, False)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 1)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 2)
+        # default behavior
+        self.assertEqual(warnings.return_check_limits(), 1)
+
+        # to set minimum we need to make maximum higher
+        warnings.set_maximum(10)
+        for x in range(1, 10):
+            if x <= 3:
+                self.assertEqual(warnings.return_check_limits(), 0)
+            else:
+                self.assertEqual(warnings.return_check_limits(), 1)
+            warnings.set_minimum(x)
+
+
