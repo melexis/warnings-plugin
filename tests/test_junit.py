f9e7b9174ee81d2c2b4bcf936d6faa5507b1542c
Stein Heselmans <teh@melexis.com>
2017-06-30 10:10:19 +0200

    Adapt some tests (xml file based) for junit

--- tests/test_junit.py
+++ tests/test_junit.py
@@ -8,24 +8,14 @@ class TestJUnitFailures(TestCase):
         self.warnings = WarningsPlugin(False, False, True)
 
     def test_no_warning(self):
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name" />')
+        self.warnings.check_file('tests/junit_no_fail.xml')
         self.assertEqual(self.warnings.return_count(), 0)
 
     def test_single_warning(self):
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name"><failure message="some random message from test case" /></testcase>')
-        self.assertEqual(self.warnings.return_count(), 1)
-
-    def test_single_warning_with_random_spaces(self):
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name"> <   failure   message ="some random message from test case" /></testcase>')
-        self.assertEqual(self.warnings.return_count(), 1)
-
-    def test_single_warning_mixed(self):
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name1" />')
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name2"><failure message="some random message from test case" /></testcase>')
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name3" />')
+        self.warnings.check_file('tests/junit_single_fail.xml')
         self.assertEqual(self.warnings.return_count(), 1)
 
     def test_dual_warning(self):
-        self.warnings.check('<testcase classname="dummy_class" name="dummy_name1"><failure message="some random message from test case 1" /></testcase><testcase classname="dummy_class" name="dummy_name2"><failure message="some random message from test case 2" /></testcase>')
+        self.warnings.check_file('tests/junit_double_fail.xml')
         self.assertEqual(self.warnings.return_count(), 2)
 
