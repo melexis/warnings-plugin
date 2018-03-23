b7f42465253da7a23678b11419472f2e21e3c664
Crt Mori <cmo@melexis.com>
2018-03-23 13:49:53 +0100

    Update unit tests for multiple arguments

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -56,4 +56,51 @@ class TestWarningsPlugin(TestCase):
         warnings.check('This should not be treated as warning2')
         self.assertEqual(warnings.return_count(), 1)
 
+    def test_doxy_sphinx_warning(self):
+        warnings = WarningsPlugin(True, True, False)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 1)
+        warnings.check("/home/bljah/test/index.rst:5: WARNING: toctree contains reference to nonexisting document u'installation'")
+        self.assertEqual(warnings.return_count(), 2)
+        with open('tests/junit_single_fail.xml') as xmlfile:
+            warnings.check(xmlfile.read())
+        self.assertEqual(warnings.return_count(), 2)
+        warnings.check('This should not be treated as warning2')
+        self.assertEqual(warnings.return_count(), 2)
+
+    def test_doxy_junit_warning(self):
+        warnings = WarningsPlugin(False, True, True)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 1)
+        warnings.check("/home/bljah/test/index.rst:5: WARNING: toctree contains reference to nonexisting document u'installation'")
+        self.assertEqual(warnings.return_count(), 1)
+        with open('tests/junit_single_fail.xml') as xmlfile:
+            warnings.check(xmlfile.read())
+        self.assertEqual(warnings.return_count(), 2)
+        warnings.check('This should not be treated as warning2')
+        self.assertEqual(warnings.return_count(), 2)
+
+    def test_sphinx_junit_warning(self):
+        warnings = WarningsPlugin(True, False, True)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 0)
+        warnings.check("/home/bljah/test/index.rst:5: WARNING: toctree contains reference to nonexisting document u'installation'")
+        self.assertEqual(warnings.return_count(), 1)
+        with open('tests/junit_single_fail.xml') as xmlfile:
+            warnings.check(xmlfile.read())
+        self.assertEqual(warnings.return_count(), 2)
+        warnings.check('This should not be treated as warning2')
+        self.assertEqual(warnings.return_count(), 2)
+
+    def test_all_warning(self):
+        warnings = WarningsPlugin(True, True, True)
+        warnings.check('testfile.c:6: warning: group test: ignoring title "Some test functions" that does not match old title "Some freaky test functions"')
+        self.assertEqual(warnings.return_count(), 1)
+        warnings.check("/home/bljah/test/index.rst:5: WARNING: toctree contains reference to nonexisting document u'installation'")
+        self.assertEqual(warnings.return_count(), 2)
+        with open('tests/junit_single_fail.xml') as xmlfile:
+            warnings.check(xmlfile.read())
+        self.assertEqual(warnings.return_count(), 3)
+        warnings.check('This should not be treated as warning2')
+        self.assertEqual(warnings.return_count(), 3)
 
