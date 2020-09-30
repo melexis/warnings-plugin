0bd07d87c2d0c0b7d6f2d7e885ff0e1c24013c02
jce <jce@melexis.com>
2020-09-30 17:57:33 +0200

    Add initial commit for Robot Framework Checker

--- src/mlx/warnings_checker.py
+++ src/mlx/warnings_checker.py
@@ -19,6 +19,8 @@ xmlrunner_pattern = re.compile(PYTHON_XMLRUNNER_REGEX)
 COVERITY_WARNING_REGEX = r"(?:((?:[/.]|[A-Za-z]).+?):(-?\d+):) (CID) \d+ \(#(?P<curr>\d+) of (?P<max>\d+)\): (?P<checker>.+)\): (?P<classification>\w+), *(.+)\n?"
 coverity_pattern = re.compile(COVERITY_WARNING_REGEX)
 
+robot_pattern = re.compile('.*')  # TODO
+
 
 class WarningsChecker:
     name = 'checker'
@@ -289,3 +291,106 @@ class CoverityChecker(RegexChecker):
                     (match.group('classification') in self.CLASSIFICATION):
                 self.count += 1
                 self.print_when_verbose(match.group(0).strip())
+
+
+class RobotChecker(WarningsChecker):
+    name = 'robot'
+    suite_checkers = []
+
+    def get_minimum(self):
+        ''' Gets the lowest minimum amount of warnings
+
+        Returns:
+            int: the lowest minimum for warnings
+        '''
+        if self.suite_checkers:
+            return min(x.get_minimum() for x in self.suite_checkers)
+        return 0
+
+    def set_minimum(self, minimum):
+        ''' Setter function for the minimum amount of warnings
+
+        Args:
+            minimum (int): minimum amount of warnings allowed
+        '''
+        for checker in self.suite_checkers:
+            checker.set_minimum(minimum)
+
+    def get_maximum(self):
+        ''' Gets the highest minimum amount of warnings
+
+        Returns:
+            int: the highest maximum for warnings
+        '''
+        if self.suite_checkers:
+            return max(x.get_maximum() for x in self.suite_checkers)
+        return 0
+
+    def set_maximum(self, maximum):
+        ''' Setter function for the maximum amount of warnings
+
+        Args:
+            maximum (int): maximum amount of warnings allowed
+        '''
+        for checker in self.suite_checkers:
+            checker.set_maximum(maximum)
+
+    def check(self, content):
+        '''
+        Function for counting the number of failures in a specific Robot
+        Framework test suite
+
+        Args:
+            content (str): The content to parse
+        '''
+        for checker in self.suite_checkers:
+            checker.check(content)
+
+    def return_check_limits(self):
+        ''' Function for checking whether the warning count is within the configured limits
+
+        Returns:
+            int: 0 if the amount of warnings is within limits, the count of warnings otherwise
+                (or 1 in case of a count of 0 warnings)
+        '''
+        count = 0
+        for checker in self.suite_checkers:
+            count += checker.return_check_limits()
+        return count
+
+    def parse_suites_config(self, suite_configs):
+        self.suite_checkers = []
+        for config in suite_configs:
+            checker = RobotSuiteChecker(config['name'])
+            checker.set_maximum(int(config['max']))
+            checker.set_minimum(int(config['min']))
+            self.suite_checkers.append(checker)
+
+
+class RobotSuiteChecker(RegexChecker):
+    name = 'suite name'
+    pattern = robot_pattern
+
+    def __init__(self, name, **kwargs):
+        ''' Constructor
+
+        Args:
+            name: Name of the test suite to check the results of
+        '''
+        super().__init__(**kwargs)
+        self.name = name
+
+    def check(self, content):
+        ''' Function for counting the number of failures for a suite in a robot test report
+
+        Args:
+            content (str): The content to parse
+        '''
+        # TODO
+        matches = re.finditer(self.pattern, content)
+        for match in matches:
+            match_string = match.group(0).strip()
+            if self._is_excluded(match_string):
+                continue
+            self.count += 1
+            self.print_when_verbose(match_string)
