25ae6b0822236c3b835041fcc453470f936ba834
Jasper Craeghs <jasper.craeghs@gmail.com>
2020-10-02 20:13:42 +0200

    Enable verbose flag and assert prints

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -12,8 +12,8 @@ class TestRobotWarnings(TestCase):
         self.suite1 = 'Suite One'
         self.suite2 = 'Suite Two'
         self.dut.checkers = [
-            RobotSuiteChecker(self.suite1),
-            RobotSuiteChecker(self.suite2),
+            RobotSuiteChecker(self.suite1, verbose=True),
+            RobotSuiteChecker(self.suite2, verbose=True),
         ]
 
     def test_no_warning(self):
@@ -32,7 +32,7 @@ class TestRobotWarnings(TestCase):
         self.assertIn("Suite {!r}: 1 warnings found".format(self.suite1), stdout_log)
         self.assertIn("Suite {!r}: 0 warnings found".format(self.suite2), stdout_log)
 
-    def test_double_warning(self):
+    def test_double_warning_and_verbosity(self):
         with open('tests/robot_double_fail.xml', 'r') as xmlfile:
             with patch('sys.stdout', new=StringIO()) as fake_out:
                 self.warnings.check(xmlfile.read())
@@ -40,8 +40,15 @@ class TestRobotWarnings(TestCase):
         stdout_log = fake_out.getvalue()
 
         self.assertEqual(count, 2)
-        self.assertIn("Suite {!r}: 1 warnings found".format(self.suite1), stdout_log)
-        self.assertIn("Suite {!r}: 1 warnings found".format(self.suite2), stdout_log)
+        self.assertEqual(
+            '\n'.join([
+                "Suite One &amp; Suite Two.Suite One.First Test",
+                "Suite One &amp; Suite Two.Suite Two.Another test",
+                "Suite {!r}: 1 warnings found".format(self.suite1),
+                "Suite {!r}: 1 warnings found".format(self.suite2),
+            ]) + '\n',
+            stdout_log
+        )
 
     def test_invalid_xml(self):
         self.warnings.check('this is not xml')
