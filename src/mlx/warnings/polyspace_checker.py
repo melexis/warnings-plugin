8b580406a0048a5271070743eef776ee2d78631c
JWM <jwm@melexis.com>
2024-06-26 16:13:55 +0200

    Make PolyspaceCheck inherit from Warningschecker

--- src/mlx/warnings/polyspace_checker.py
+++ src/mlx/warnings/polyspace_checker.py
@@ -8,12 +8,50 @@ class PolyspaceChecker(WarningsChecker):
     checkers = []
 
     @property
-    def counted_warnings(self, name):
+    def counted_warnings(self):
         ''' List: list of counted warnings (str) '''
         all_counted_warnings = []
-        for checker in self.checkers[name]:
+        for checker in self.checkers:
             all_counted_warnings.extend(checker.counted_warnings)
         return all_counted_warnings
+
+    def get_minimum(self):
+        ''' Gets the lowest minimum amount of warnings
+
+        Returns:
+            int: the lowest minimum for warnings
+        '''
+        if self.checkers:
+            return min(x.get_minimum() for x in self.checkers)
+        return 0
+
+    def set_minimum(self, minimum):
+        ''' Setter function for the minimum amount of warnings
+
+        Args:
+            minimum (int): minimum amount of warnings allowed
+        '''
+        for checker in self.checkers:
+            checker.set_minimum(minimum)
+
+    def get_maximum(self):
+        ''' Gets the highest minimum amount of warnings
+
+        Returns:
+            int: the highest maximum for warnings
+        '''
+        if self.checkers:
+            return max(x.get_maximum() for x in self.checkers)
+        return 0
+
+    def set_maximum(self, maximum):
+        ''' Setter function for the maximum amount of warnings
+
+        Args:
+            maximum (int): maximum amount of warnings allowed
+        '''
+        for checker in self.checkers:
+            checker.set_maximum(maximum)
 
     def check(self, content, **kwargs):
         '''
@@ -21,7 +59,6 @@ class PolyspaceChecker(WarningsChecker):
 
         Args:
             content (_io.TextIOWrapper): The open file to parse
-            file_extension (str): The file extenstion
         '''
         if kwargs["file_extension"] == ".tsv":
             reader = csv.DictReader(content, delimiter="\t")
@@ -35,6 +72,11 @@ class PolyspaceChecker(WarningsChecker):
                 if row['family'].lower() == checker.family_value:
                     if row[checker.column_name].lower() == checker.check_value:
                         checker.count = checker.count + 1
+                        checker.counted_warnings.append('family: {} -> {}: {}'.format(
+                            checker.family_value,
+                            checker.column_name,
+                            checker.check_value
+                        ))
 
     def return_count(self):
         ''' Getter function for the amount of warnings found
@@ -44,8 +86,7 @@ class PolyspaceChecker(WarningsChecker):
         '''
         self.count = 0
         for checker in self.checkers:
-            print("{0.count} warnings found for '{0.column_name}: {0.check_value}'".format(checker))
-            self.count += checker.count
+            self.count += checker.return_count()
         return self.count
 
     def return_check_limits(self):
@@ -55,28 +96,14 @@ class PolyspaceChecker(WarningsChecker):
             int: 0 if the amount of warnings is within limits, the count of warnings otherwise
                 (or 1 in case of a count of 0 warnings)
         '''
-        check_failures = 0
-        if len(self.checkers) == 0:
-            print("Warning: There are no polyspace checks recognised. Please specify them in a configuration file.")
+        count = 0
         for checker in self.checkers:
-            print('Counted failures for {!r}: {!r}.'.format(checker.column_name, checker.check_value))
-            self.count = checker.count
-            self.warn_min = checker.minimum
-            self.warn_max = checker.maximum
-            if checker.count > checker.maximum or checker.count < checker.minimum:
-                check_failures += 1
-                self._return_error_code()
-            elif self.warn_min == self.warn_max and self.count == self.warn_max:
-                print("Number of warnings ({0.count}) is exactly as expected. Well done.".format(self))
-            else:
-                print(
-                    "Number of warnings ({0.count}) is between limits {0.warn_min} and {0.warn_max}. Well done."
-                    .format(self)
-                )
-        self.warn_min = 0
-        self.warn_max = 0
-        self.count = check_failures
-        return check_failures
+            print(
+                'Counted failures for family {!r} \'{}: {}\''
+                .format(checker.family_value, checker.column_name, checker.check_value)
+            )
+            count += checker.return_check_limits()
+        return count
 
     def parse_config(self, config):
         """Parsing configuration dict extracted by previously opened JSON or yaml/yml file
@@ -96,31 +123,26 @@ class PolyspaceChecker(WarningsChecker):
                 continue
             for check in data:
                 for key, value in check.items():
-                    if key == "min":
-                        minimum = value
-                    elif key == "max":
-                        maximum = value
-                    else:
-                        column_name = key.lower()
-                        check_value = value.lower()
-                if not minimum:
-                    minimum = 0
-                if not maximum:
-                    maximum = 0
+                    if key in ["min", "max"]:
+                        continue
+                    column_name = key.lower()
+                    check_value = value.lower()
+                    checker = PolyspaceCheck(family_value, column_name, check_value, verbose=self.verbose)
+                    checker.parse_config(check)
+                    self.checkers.append(checker)
                 if not (column_name and check_value):
                     raise ValueError(
                         "Expected a list of dicts as value of the key which represents the value of the "
                         "'Family' column. These dicts need to consist 3 key-value pairs (Note: if 'min' or "
                         "'max' is not defined, it will get the default value of 0):\n"
-                        "{\n    <column-name>: <value_to_check>,\n    min: <number>,\n    max: <number>\n};"
-                        f"got\n{{\n    {column_name}: {check_value},\n    min: {minimum},\n    max: {maximum}\n}}\n"
+                        "{\n    <column_name>: <value_to_check>,\n    min: <number>,\n    max: <number>\n};"
+                        f"got {column_name} as column_name and {check_value} as value_to_check"
                     )
-                self.checkers.append(PolyspaceCheck(family_value, column_name, check_value, minimum, maximum))
 
 
-class PolyspaceCheck:
+class PolyspaceCheck(WarningsChecker):
 
-    def __init__(self, family_value, column_name, check_value, minimum, maximum):
+    def __init__(self, family_value, column_name, check_value, **kwargs):
         """Initialize the PolyspaceCheck
 
         Args:
@@ -130,25 +152,16 @@ class PolyspaceCheck:
             minimum (int): The minimum amount the check_value can occur
             maximum (int): The maximum amount the check_value can occur
         """
+        super().__init__(**kwargs)
         self.family_value = family_value
         self.column_name = column_name
         self.check_value = check_value
-        self.minimum = minimum
-        self.maximum = maximum
-        self.count = 0
 
-    def get_minimum(self):
-        ''' Gets the lowest minimum amount of warnings
+    def return_count(self):
+        ''' Getter function for the amount of warnings found
 
         Returns:
-            int: the lowest minimum for warnings
+            int: Number of warnings found
         '''
-        return self.minimum
-
-    def get_maximum(self):
-        ''' Gets the highest minimum amount of warnings
-
-        Returns:
-            int: the highest maximum for warnings
-        '''
-        return self.maximum
+        print("{} warnings found".format(self.count))
+        return self.count
