7776426c7e4403fce865ca4ff879cfad3b556253
JWM <jwm@melexis.com>
2024-06-26 13:14:59 +0200

    Add test with csv file (code prover)

--- tests/test_polyspace.py
+++ tests/test_polyspace.py
@@ -29,6 +29,16 @@ class TestPolyspaceWarnings(unittest.TestCase):
         self.assertIn(f"{self.dut.checkers[0].count} warnings found for '{self.dut.checkers[0].column_name}: {self.dut.checkers[0].check_value}'", stdout_log)
         self.assertIn(f"{self.dut.checkers[1].count} warnings found for '{self.dut.checkers[1].column_name}: {self.dut.checkers[1].check_value}'", stdout_log)
 
+    def test_code_prover_csv_file(self):
+        with open('tests/test_in/polyspace_code_prover.csv', newline="") as file:
+            with patch('sys.stdout', new=StringIO()) as fake_out:
+                self.warnings.check(file, file_extension='.csv')
+                count = self.warnings.return_count()
+        stdout_log = fake_out.getvalue()
+
+        self.assertEqual(count, 169)
+        self.assertIn(f"{self.dut.checkers[0].count} warnings found for '{self.dut.checkers[0].column_name}: {self.dut.checkers[0].check_value}'", stdout_log)
+        self.assertIn(f"{self.dut.checkers[1].count} warnings found for '{self.dut.checkers[1].column_name}: {self.dut.checkers[1].check_value}'", stdout_log)
 
 if __name__ == '__main__':
     unittest.main()
