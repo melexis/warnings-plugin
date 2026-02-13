c3ba7b28d9be5086f383ff725bfd4059ef616bec
Crt Mori <cmo@melexis.com>
2026-02-13 15:36:03 +0100

    MD5 hashing is used for fingerprinting
    Clearly state that it is not used for security, so that Static Analysis tools do no flag a false positive.

--- src/mlx/warnings/code_quality.py
+++ src/mlx/warnings/code_quality.py
@@ -32,9 +32,9 @@ class Finding:
         4. Step 3 is repeated until a unique hash is obtained.
         """
         hashable_string = f"{self.severity}{self.path}{self.description}"
-        new_hash = hashlib.md5(str(hashable_string).encode("utf-8")).hexdigest()
+        new_hash = hashlib.md5(str(hashable_string).encode("utf-8"), usedforsecurity=False).hexdigest()
         while new_hash in self.fingerprints and self.fingerprints[new_hash] != self:
-            new_hash = hashlib.md5(f"{hashable_string}{new_hash}".encode()).hexdigest()
+            new_hash = hashlib.md5(f"{hashable_string}{new_hash}".encode(), usedforsecurity=False).hexdigest()
         type(self).fingerprints[new_hash] = self
         return new_hash
 
